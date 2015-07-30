f87f41c14b139254285d67b16a2b9c85ab8d563f
Matt Sanford <matt@mzsanford.com>
2015-07-30 14:39:21 -0700

    Remove jquery calls

--- shared/js/safari.js
+++
@@ -1,7 +0,0 @@
-
-// Safari background functions
-safari.self.addEventListener('message', function(msg) {
-  if (msg.name == 'addToDash') {
-    $('.btn-add-to-dash:first').click();
-  }
-}, false);
